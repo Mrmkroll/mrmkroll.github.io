d07c5d520cbed6af8112e99995fa33f9340f26ba
Mrmk <mrmkroll@gmail.com>
2021-02-14 02:00:25 +0900

    Edit files

---
+++ js/disable-developertools.js
@@ -0,0 +1,23 @@
+$(function () {
+    $(document).keydown(function (event) {
+        var keyCode = event.keyCode;
+
+        var ctrlClick = event.ctrlKey;
+        
+        var shiftClick = event.shiftKey;
+
+        if (keyCode == 111 || keyCode == 123) {
+            return false;
+        }
+
+        // mac
+        if ((keyCode == 55 || keyCode == 56) && (keyCode == 58 || keyCode == 61) && (keyCode == 8 || keyCode == 34)) {
+            return false;
+        }
+
+        // Windows
+        if (shiftClick && ctrlClick && (keyCode == 67 || keyCode == 73)){
+            return false;
+        }
+    });
+});
